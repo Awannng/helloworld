4a167c7a57615c8cde9d9ce4429243890182cc65
Jing Wang <jingwangw12@gmail.com>
2024-11-16 00:45:29 -0500

    minor change to removePin function, but still had some small issue

--- src/components/AddPin.jsx
+++ src/components/AddPin.jsx
@@ -8,6 +8,7 @@ const AddPin = ({ pins, setPins }) => {
     click(e) {
       //destructure the lat and lng from e
       const { lat, lng } = e.latlng;
+
       // add the location into pins plus any previous added pins
       setPins((prevPins) => [...prevPins, { lat, lng }]);
 
@@ -20,39 +21,41 @@ const AddPin = ({ pins, setPins }) => {
     e.target.openPopup();
   };
 
-  //Deletes the marker when click on the "delete" button, this filters the pin where the does not equal to the pin we delete
-  //issue with pins other place when click on delete
-  const removePin = (pos) => {
-    setPins((prevPins) =>
-      prevPins.filter((pins) => JSON.stringify(pins) !== JSON.stringify(pos))
+  //Deletes the marker when click on the "delete" button, this filters the pin where the lat or lon are not equal
+  //issue: pins other place when click on delete
+  const removePin = (pin) => {
+    setPins(
+      (prevPins) =>
+        prevPins.filter((p) => p.lat !== pin.lat || p.lng !== pin.lng) // Remove the specific pin
     );
   };
   return (
     <>
       {/* display the pins on the map */}
-      {pins
-        ? pins.map((pin, idx) => (
-            <Marker
-              key={idx}
-              position={pin}
-              eventHandlers={{
-                add: openPopup,
-              }}
-            >
-              <Popup>
-                {/* when click of the pin, it will show a pop up form that store the info of the palnned travel by aksing Country, Start Date, End Date */}
-                <button
-                  className=" text-red-700 mb-2"
-                  onClick={() => removePin(pin)}
-                  type="button"
-                >
-                  Delete
-                </button>
-                <PopupForm pos={pin} pins={pins} setPins={setPins} />
-              </Popup>
-            </Marker>
-          ))
-        : null}
+      {pins.length > 0 &&
+        pins.map((pin, idx) => (
+          <Marker
+            key={idx}
+            position={pin}
+            eventHandlers={{
+              add: openPopup,
+            }}
+          >
+            <Popup>
+              {/* Delete pin button */}
+              <button
+                className=" text-red-700 mb-2"
+                onClick={() => removePin(pin)}
+                type="button"
+              >
+                Delete
+              </button>
+
+              {/* when click of the pin, it will show a pop up form that store the info of the palnned travel by aksing Country, Start Date, End Date */}
+              <PopupForm pos={pin} pins={pins} setPins={setPins} />
+            </Popup>
+          </Marker>
+        ))}
     </>
   );
 };
