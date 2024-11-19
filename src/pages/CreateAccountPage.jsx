f27a149548e27e8410a1cf2035ea9612465d92cb
Jing Wang <jingwangw12@gmail.com>
2024-11-18 20:08:25 -0500

    changed the style of the login page

--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -12,85 +12,88 @@ const CreateAccountPage = ({}) => {
 
   return (
     <>
-      <form>
-        {/* Centers the form */}
-        <div className="flex flex-col mr-auto ml-auto justify-center w-3/12 min-h-svh gap-5">
-          <div className="flex flex-col">
-            {/* Input box for username */}
-            <label htmlFor="username">Username</label>
-            <input
-              className="mb-1 border"
-              type="text"
-              value={username}
-              name="username"
-              id="username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-              required
-            />
-          </div>
+      <div className="log-in-bg">
+        <form>
+          {/* Centers the form */}
 
-          {/* Input box for password */}
-          <div className="flex flex-col">
-            {/* Show the requirment of the password in gray */}
-            <p className="text-sm text-slate-500">
-              Contains digits from 1 to 9 <br />
-              One lowercase letter <br />
-              One uppercase letter <br />
-              One special character <br />
-              No space, 8-16 characters long
-            </p>
-            <label htmlFor="password">Password</label>
-            <input
-              className="mb-1 border"
-              type="text"
-              value={password}
-              name="password"
-              id="password"
-              pattern="/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              required
-            />
-          </div>
+          <div className="flex flex-col mr-auto ml-auto justify-center w-3/12 min-h-svh gap-5">
+            <div className="flex flex-col">
+              {/* Input box for username */}
+              <label htmlFor="username">Username</label>
+              <input
+                className="mb-1 border"
+                type="text"
+                value={username}
+                name="username"
+                id="username"
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                required
+              />
+            </div>
 
-          {/* Input box for comfirming password */}
-          <div className="flex flex-col">
-            <label htmlFor="confirm">Confirm Password</label>{" "}
-            <input
-              className="mb-1 border"
-              type="text"
-              value={confirm}
-              name="confirm"
-              id="confirm"
-              onChange={(e) => {
-                setConfirm(e.target.value);
-              }}
-              required
-            />
-          </div>
+            {/* Input box for password */}
+            <div className="flex flex-col">
+              {/* Show the requirment of the password in gray */}
+              <p className="text-sm text-slate-500">
+                Contains digits from 1 to 9 <br />
+                One lowercase letter <br />
+                One uppercase letter <br />
+                One special character <br />
+                No space, 8-16 characters long
+              </p>
+              <label htmlFor="password">Password</label>
+              <input
+                className="mb-1 border"
+                type="text"
+                value={password}
+                name="password"
+                id="password"
+                pattern="/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                required
+              />
+            </div>
 
-          {/* Create account button to have the data send to the database */}
-          <div className="mt-1 flex flex-col gap-10 justify-center">
-            <Link>
-              <button
-                className=" bg-lime-700 text-white p-1 rounded-full "
-                type="button"
-              >
-                Create Account
-              </button>
-            </Link>
+            {/* Input box for comfirming password */}
+            <div className="flex flex-col">
+              <label htmlFor="confirm">Confirm Password</label>{" "}
+              <input
+                className="mb-1 border"
+                type="text"
+                value={confirm}
+                name="confirm"
+                id="confirm"
+                onChange={(e) => {
+                  setConfirm(e.target.value);
+                }}
+                required
+              />
+            </div>
 
-            <Link to='/login'>
-              <div className="text-blue-600">
-                Back to Login
-              </div>
-            </Link>
+            {/* Create account button to have the data send to the database */}
+            <div className="mt-1 flex flex-col gap-10 justify-center">
+              <Link>
+                <button
+                  className=" bg-orange-400 hover:bg-orange-700 text-white p-1 rounded-full "
+                  type="button"
+                >
+                  Create Account
+                </button>
+              </Link>
+
+              <Link to="/login">
+                <div className="text-blue-600">
+                  <small>Back to Login</small>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 };
