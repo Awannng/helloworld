d6130adc5e9a91b2156aad0d4e852d42d47e7d8f
Jing Wang <jingwangw12@gmail.com>
2024-11-28 14:44:44 -0500

    minor change to the profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -16,59 +16,59 @@ const ProfilePage = ({ menu, setMenu, signOut }) => {
   const [openSetting, setOpenSetting] = useState(false);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen z-0">
-      {/* Navigate back to Home Page and have setting button*/}
-      <div className="absolute top-0 left-0 z-20">
-        <button onClick={() => setMenu(!menu)}>
-          <Logo />
-        </button>
-
-        {/* When click the on the logo, it shows a dropdown menu */}
-        {menu && (
-          <div className="absolute top-5 left-5 translate-y-1/2">
-            <div className="bg-white h-auto shadow-md rounded-md text-sm flex flex-col p-1 ">
-              <button
-                // When click on the button, it goes to the home page
-                className="hover:bg-slate-100 rounded-md p-2"
-              >
-                <Link to="/home">Home</Link>
-              </button>
-
-              <button
-                //When click, it will open up a form for user to enter their info
-                className="hover:bg-slate-100 rounded-md p-2"
-                type="button"
-                onClick={() => setOpenSetting(!openSetting)}
-              >
-                Setting
-              </button>
-
-              <button
-                //When click, it will goes back to the landing page
-                onClick={signOut}
-                className="hover:bg-slate-100 rounded-md p-2"
-                type="button"
-              >
-                Log Out
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-
-      <div className="setting-box">
-        {/* props for SettingForm close button */}
-        {openSetting && (
-          <SettingForm
-            openSetting={openSetting}
-            setOpenSetting={setOpenSetting}
-          />
-        )}
-      </div>
-
-      
+    <>
       {/* Profile Page Container */}
-      <div className="flex flex-col items-center justify-start bg-purple-100 rounded-lg shadow-xl w-[90vw] h-[88vh] mt-20 overflow-hidden">
+      <div className="flex flex-col items-center justify-start bg-purple-100 w-full h-lvh overflow-hidden">
+        {/* Navigate back to Home Page and have setting button*/}
+        <div className="absolute top-0 left-0 z-20">
+          <button onClick={() => setMenu(!menu)}>
+            <Logo />
+          </button>
+
+          {/* When click the on the logo, it shows a dropdown menu */}
+          {menu && (
+            <div className="absolute top-5 left-5 translate-y-1/2">
+              <div className="bg-white h-auto shadow-md rounded-md text-sm flex flex-col p-1 ">
+                <button
+                  // When click on the button, it goes to the home page
+                  className="hover:bg-slate-100 rounded-md p-2"
+                >
+                  <Link to="/home">Home</Link>
+                </button>
+
+                <button
+                  //When click, it will open up a form for user to enter their info
+                  className="hover:bg-slate-100 rounded-md p-2"
+                  type="button"
+                  onClick={() => setOpenSetting(!openSetting)}
+                >
+                  Setting
+                </button>
+
+                <button
+                  //When click, it will goes back to the landing page
+                  onClick={signOut}
+                  className="hover:bg-slate-100 rounded-md p-2"
+                  type="button"
+                >
+                  Log Out
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* The setting form that will pop up */}
+        <div className="setting-box">
+          {/* props for SettingForm close button */}
+          {openSetting && (
+            <SettingForm
+              openSetting={openSetting}
+              setOpenSetting={setOpenSetting}
+            />
+          )}
+        </div>
+
         {/* Profile Banner (under construction) */}
         <div className="relative h-[40vh] w-full border-b-8 border-white">
           <img
@@ -79,7 +79,7 @@ const ProfilePage = ({ menu, setMenu, signOut }) => {
         </div>
 
         {/* Display Profile Image */}
-        <div className="absolute top-[40vh] z-10 bg-white w-48 h-48 rounded-full border-4 border-white flex items-center justify-center">
+        <div className="absolute top-[25vh] z-10 bg-white w-48 h-48 rounded-full border-4 border-white flex items-center justify-center">
           <img
             src={user.profileImage}
             alt="Profile Picture"
@@ -88,7 +88,7 @@ const ProfilePage = ({ menu, setMenu, signOut }) => {
         </div>
 
         {/* Display User Details */}
-        <div className="relative top-[120px]">
+        <div className="relative top-[120px] flex flex-col items-center">
           <h1 className="text-3xl font-semibold text-gray-800 mb-2">
             {user.username}
           </h1>
@@ -100,7 +100,7 @@ const ProfilePage = ({ menu, setMenu, signOut }) => {
           </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
