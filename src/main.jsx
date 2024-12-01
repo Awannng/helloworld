6e29b65e5ea7d5812f387c753831c8d9c5ed99dc
Shika Lu <zzzzz@macbook-pro-2.mynetworksettings.com>
2024-11-30 23:25:23 -0500

    update clerk sign up logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -13,8 +14,11 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/login">
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}
+  afterSignOutUrl="/">
+    <BrowserRouter>
       <App />
-    </ClerkProvider>
-  </StrictMode>
+    </BrowserRouter>
+  </ClerkProvider>
+</StrictMode>
 );
