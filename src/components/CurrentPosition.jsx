896b37310b4c3b0233c15bb951914f7f3209c05e
Jing Wang <jingwangw12@gmail.com>
2024-12-03 18:58:55 -0500

    customized currentLocation pin

--- src/components/CurrentPosition.jsx
+++ src/components/CurrentPosition.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
-import { useMapEvents, useMap, Marker, Popup } from "react-leaflet";
+import { useMap, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
 
 const CurrentPosition = ({ currentPosition, setCurrentPosition }) => {
   //allows to access the map instance directly
@@ -15,11 +17,19 @@ const CurrentPosition = ({ currentPosition, setCurrentPosition }) => {
     });
   }, [map]);
 
+  //customized a different pin for currentLocation
+  const currentLocPin = new Icon({
+    iconUrl: "/images/currentLoc.png",
+    iconSize: [60, 60], // size of the icon
+    iconAnchor: [25, 50], // Position of the icon's anchor point (relative to the icon)
+    popupAnchor: [6, -30], // Position of the popup anchor
+  });
+
   return (
     <>
       {/* place a marker on the map if currentPosition is available and a popup when clicked on the marker */}
       {currentPosition ? (
-        <Marker position={currentPosition}>
+        <Marker position={currentPosition} icon={currentLocPin}>
           <Popup>You are here</Popup>
         </Marker>
       ) : null}
